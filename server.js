78ea644b8ea3ccf8d5b1ab25e97bfac7342b3147
pouyarezvani <pouyarezvani10@gmail.com>
2021-07-30 22:15:47 -0700

    bot is working

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+let lastEnteredNumber = 56;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+
+    // let targetChanel = msg.guild.channels.cache.get('860809577595666432')
+    if (msg.channel.id == '860809577595666432') {
+        let input = msg.content;
+        const inputChecker = (input) => {
+            if (input == lastEnteredNumber + 1) {
+                lastEnteredNumber += 1;
+                return true
+            } else {
+                return false
+            }
+        }
+        if (!inputChecker(input)) msg.delete()
+    }
+});
