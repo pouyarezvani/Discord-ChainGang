ec89dc2ec1db8a76f78048c9e6f42cf626e3ab7a
pouyarezvani <pouyarezvani10@gmail.com>
2021-08-01 22:24:04 -0700

    changes

---
+++ server.ts
@@ -0,0 +1,104 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const CHAIN_GANG_CHANNEL_ID = '860809577595666432'
+const TOKEN: string = process.env.TOKEN || "";
+if (TOKEN === "") throw new Error("TOKEN env var not found");
+
+interface User {
+    tag: string
+}
+
+type EventHandler = (msg?: Message) => Promise<void>
+
+interface Bot {
+    channels: Channels
+    login: (token: string) => null
+    user: User
+    on: (event: string, handler: EventHandler) => null
+}
+
+const bot: Bot = new Discord.Client();
+
+bot.login(TOKEN);
+
+// let lastEnteredNumber: number = 0;
+
+
+function isChannelId(id: string): any {
+    return id == CHAIN_GANG_CHANNEL_ID
+}
+
+interface Messages {
+    fetch: (params: FetchParams) => Promise<FetchResult>
+}
+
+interface Channel {
+    id: string
+    messages: Messages
+}
+
+type Getter = (id: string) => Channel
+
+interface Cache {
+    get: Getter
+}
+
+interface Channels {
+    cache: Cache
+}
+
+interface FetchParams {
+    limit?: number
+}
+
+interface FetchResult {
+    first: () => Message | null
+    last: () => Message | null
+}
+
+interface Message {
+    channel: Channel
+    content: string
+    delete: Function
+    fetch: (params: FetchParams) => Promise<FetchResult>
+}
+
+const isMessageValid = async (currentMessage: Message): Promise<boolean> => {
+    let channel = currentMessage.channel;
+    const res = await channel.messages.fetch({ limit: 2 })
+    const lastMessage = res.last() || {content: "0"};
+
+    const lastEnteredNumber = parseInt(lastMessage.content);
+    console.log('lastEnteredNumber inside isMessageValid ', lastEnteredNumber);
+
+    const currentEnteredNumber = parseInt(currentMessage.content);
+    console.log('currentEnteredNumber inside isMessageValid ', currentEnteredNumber);
+
+    return currentEnteredNumber === lastEnteredNumber + 1;
+}
+
+
+bot.on('ready', async () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+    // try {
+    //     let channel = bot.channels.cache.get(CHAIN_GANG_CHANNEL_ID);
+    //     const res = await channel.messages.fetch({ limit: 1 })
+    //     let lastMessage = res.first();
+    //     lastEnteredNumber = parseInt(lastMessage.content);
+    //     console.log('lastEnteredNumber inside ready: ', lastEnteredNumber)
+    // } catch (e) {
+    //     console.log(e)
+    // }
+});
+
+bot.on('message', async (msg?: Message) => {
+    if (!msg) {
+        console.log("on message handler had no message. this is an error.");
+        return;
+    }
+    if (isChannelId(msg.channel.id)) {
+        console.log('user input in the channel', msg.content)
+        let isValid = await isMessageValid(msg);
+        if (!isValid) msg.delete()
+    }
+});
